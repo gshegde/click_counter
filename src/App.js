4b3fc39c0cb788626724821491f22b4ad5c5b9bb
Goutam <goutamhegde393@gmail.com>
2020-04-30 14:54:31 +0530

    counterApp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react';
+import styles from './App.module.css';
+import { Typography, Box } from '@material-ui/core';
+const App = () => {
+  const [getCount, setCount] = useState(0);
+  const clickHandler = () => setCount(getCount + 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <div>
+      <Box display="flex" flexDirection="column" alignContent="center" alignItems="center" boxShadow={6} style={{
+        width: "200px ",
+        height: "200px",
+        marginLeft: "600px",
+        marginTop: "50px",
+        borderRadius: 6,
+        boxshadow: "5px 10px"
+
+      }}>
+
+        <h2> {getCount} </h2>
+        <p onClick={clickHandler}>Click here to increment</p>
+
+      </Box>
+    </div >
+
+  )
 }
+
+
 
 export default App;
